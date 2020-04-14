81dd569435ba5dddfb99f2d199efacf60a9b2fd2
Antonio Gambogi <antonio@mf.local>
2020-04-13 22:10:10 -0300

    aulajsdio

---
+++ js/main.js
@@ -0,0 +1,85 @@
+
+function cliclou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+  //  console.log( document.getElementById("agradecimento"));
+
+  //  alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://www.google.com");
+  //  window.location.href = "https://www.google.com";
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+ //   alert("trocar texto");
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+//
+//function soma(n1, n2) {
+//    return n1+n2;
+//}
+//alert(soma(5, 10));
+
+/*
+let d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+//let count;
+//for(count=0; count <=5; count++){
+//    alert(count);
+//}
+
+/*
+let count = 0;
+while (count <= 5){
+    console.log(count);
+    count++;
+}
+*/
+
+/*
+let idade = prompt("Qual sua idade?");
+if(idade >= 18){
+    alert("maior de idade");
+} else{
+    alert("enor de idade");
+}
+*/
+
+//var frutas = [{nome:"banana", cor:"amarela"}, {nome:"uva", cor:"verde"}]
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+/*
+var lista = ["Banana", "Laranja", "Uva"];
+lista.push("Morango");
+//lista.pop();
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+
+
+/*
+let nome = "Antonio Gambogi";
+let idade = 31;
+let idade2 = 10;
+let frase = "Canadá é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Canadá", "Brasil"));
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+*/
